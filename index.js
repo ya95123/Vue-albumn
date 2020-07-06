e39cb5f8cfaf41f15abab37c0e8052134b7b73dc
ya95123 <ya95123@gmail.com>
2020-07-06 14:19:22 +0800

    fix：web serve

--- index.js
+++ index.js
@@ -22,15 +22,20 @@ const app = express()
 app.use(bodyParser.json())
 app.use(cors({
   origin (origin, callback) {
-    if (process.env.ALLOW_CORS === 'true') {
-      // 開發環境，允許
-      callback(null, true)
-    } else if (origin.includes('github')) {
-      // 非開發環境，但是從 github 過來，允許
+    // 直接開網頁，不是 ajax 時，origin 是 undefined
+    if (origin === undefined) {
       callback(null, true)
     } else {
-      // 不是開發也不是從 github 過來，拒絕
-      callback(new Error('Not allowed'), false)
+      if (process.env.ALLOW_CORS === 'true') {
+        // 開發環境，允許
+        callback(null, true)
+      } else if (origin.includes('github')) {
+        // 非開發環境，但是從 github 過來，允許
+        callback(null, true)
+      } else {
+        // 不是開發也不是從 github 過來，拒絕
+        callback(new Error('Not allowed'), false)
+      }
     }
   },
   // 取得認證資訊
